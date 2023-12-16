d78d16a2c7c76ad4540bd90dedf4ebbc27ddbacd
Tenisamuel <152555308+Tenisamuel@users.noreply.github.com>
2023-12-16 12:50:02 +0000

    Update KFC.js

--- KFC.js
+++ KFC.js
@@ -1,1 +1,2 @@
 FC Corporation, doing business as Kentucky Fried Chicken, is an American fast food restaurant chain headquartered in Louisville, Kentucky, that specializes in fried chicken. 
+They are famous for their chicken bucket
